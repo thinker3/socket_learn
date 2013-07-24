7257fe01ef58924836d987a725d2f02f76265f1f
thinker3 <metathinkerk@gmail.com>
2013-07-24 11:06:17 +0800

    when two ports working, ctrl+c does not work

--- listen_two_ports.py
+++ listen_two_ports.py
@@ -1,15 +1,19 @@
 from threading import Thread
-from SocketServer import ThreadingMixIn
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
-
 def serve_on_port(port):
-    server = ThreadingHTTPServer(("", port), SimpleHTTPRequestHandler)
-    server.serve_forever()
+    server = HTTPServer(("", port), SimpleHTTPRequestHandler)
+# try except KeyboardInterrupt does not work
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        exit()
 
 Thread(target=serve_on_port, args=[9999]).start()
+print 8888
 serve_on_port(8888)
+# with out Thread, the following does not run
+#print 9999
+#serve_on_port(9999)
 
