0257cc33f4ef5fb49940f2bbc6fde590f2261676
thinker3 <metathinkerk@gmail.com>
2013-07-24 13:47:09 +0800

    SocketServer_src

--- inspect_src.py
+++ inspect_src.py
@@ -5,6 +5,7 @@ syspath.append('/'.join(syspath[0].split('/')[:-1]))
 from base.retrospect import get_source_code
 import SimpleHTTPServer
 import BaseHTTPServer
+import SocketServer
 
 def print_write_src(obj, name):
     src = get_source_code(obj)
@@ -15,4 +16,5 @@ def print_write_src(obj, name):
     f.write(src)
     f.close()
 
-print_write_src(BaseHTTPServer, 'BaseHTTPServer')
+#print_write_src(BaseHTTPServer, 'BaseHTTPServer')
+print_write_src(SocketServer, 'SocketServer')
