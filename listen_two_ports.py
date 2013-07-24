e4c5b523b55e6ec0c0add21f6cc91ad08bb39e57
thinker3 <metathinkerk@gmail.com>
2013-07-24 10:54:19 +0800

    mini

--- listen_two_ports.py
+++ listen_two_ports.py
@@ -1,21 +1,15 @@
 from threading import Thread
 from SocketServer import ThreadingMixIn
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("Hello World!")
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
     pass
 
 def serve_on_port(port):
-    server = ThreadingHTTPServer(("localhost",port), Handler)
+    server = ThreadingHTTPServer(("", port), SimpleHTTPRequestHandler)
     server.serve_forever()
 
-Thread(target=serve_on_port, args=[1111]).start()
-serve_on_port(2222)
+Thread(target=serve_on_port, args=[9999]).start()
+serve_on_port(8888)
 
