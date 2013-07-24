add62be6b6657d6b87c9d3be8803717817ccee01
thinker3 <metathinkerk@gmail.com>
2013-07-24 09:55:32 +0800

    mini

--- simple_http_server.py
+++ simple_http_server.py
@@ -1,21 +1,14 @@
 import sys
-import BaseHTTPServer
+from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
-
-
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass = BaseHTTPServer.HTTPServer
-Protocol = "HTTP/1.0"
 
 if sys.argv[1:]: # slicing can prevent index out of range
     port = int(sys.argv[1])
 else:
-    port = 8000
-    server_address = ('127.0.0.1', port)
+    port = 9999
 
-    HandlerClass.protocol_version = Protocol
-    httpd = ServerClass(server_address, HandlerClass)
-
-    sa = httpd.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
-    httpd.serve_forever()
+server_address = ('127.0.0.1', port)
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+socketname = httpd.socket.getsockname()
+print "Serving HTTP on", socketname[0], "port", socketname[1], "..."
+httpd.serve_forever()
