4a978c68eb2ac86edaad797794128563c62c7bf2
thinker3 <metathinkerk@gmail.com>
2013-07-31 20:33:58 +0800

    https, 443

--- connect_ex.py
+++ connect_ex.py
@@ -2,8 +2,9 @@ import socket
 
 baidu = ('www.baidu.com', 80)
 netease = ('www.163.com', 80)
-twitter = ('twitter.com', 80)
-
+twitter = ('twitter.com', 443)
+facebook = ('www.facebook.com', 80)
+bank = ('ibsbjstar.ccb.com.cn', 443)
 def connect_test(host):
     try:
         soc = socket.socket(2)
@@ -15,5 +16,7 @@ def connect_test(host):
         print 'cable error'
 
 connect_test(baidu)
+connect_test(facebook)
 connect_test(netease)
+connect_test(bank)
 connect_test(twitter)
