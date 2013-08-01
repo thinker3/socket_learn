f47bed708799b2ed65924f53ddff5da23928f456
thinker3 <metathinkerk@gmail.com>
2013-08-01 22:10:55 +0800

    time out, socket.error

--- select_learn.py
+++ select_learn.py
@@ -6,22 +6,34 @@ goagent = ('127.0.0.1', 8088)
 baidu = ('www.baidu.com', 80)
 facebook = ('www.facebook.com', 443)
 bank = ('ibsbjstar.ccb.com.cn', 443)
+twitter = ('twitter.com', 443)
+weibo = ('weibo.com', 80)
 
 def test(addr):
     try:
-        sock = socket.create_connection(addr, 5)
-        readable, writable, exceptional = select.select([sock], [sock], [sock], 5)
+        sock = socket.create_connection(addr, 3)
+        print sock
+        readable, writable, exceptional = select.select([sock], [sock], [sock], 9)
         message = "GET / HTTP/1.1\r\n\r\n"
         sock.sendall(message)
         data = sock.recv(4096)
         print addr, data
-    except:
+    except socket.timeout:
         print 'time out testing %s:%d' % addr
+    except socket.error, e:
+        print 'error %s' % e
     finally:
         print
+        try:
+            sock.close()
+        except:
+            pass
 
+test(twitter)
+test(weibo)
 test(simpleserver)
 test(goagent)
+test(twitter)
 test(baidu)
-test(facebook)
 test(bank)
+test(facebook)
