4995ef7cb465d84a069ad44c48f89e06e609e361
thinker3 <metathinkerk@gmail.com>
2013-08-01 15:40:43 +0800

    test if connectable by sending and receiving

--- select_learn.py
+++ select_learn.py
@@ -1,11 +1,27 @@
 import select
 import socket
 
-addr = ('127.0.0.1', 9999)
-server = socket.create_connection(addr, 5)
-inputs = [server]
-outpust = []
-r, w, e = select.select(inputs, outpust, inputs, 5) 
-for one in (r, w, e, outpust):
-    print one
+simpleserver = ('127.0.0.1', 9999)
+goagent = ('127.0.0.1', 8088)
+baidu = ('www.baidu.com', 80)
+facebook = ('www.facebook.com', 443)
+bank = ('ibsbjstar.ccb.com.cn', 443)
 
+def test(addr):
+    try:
+        sock = socket.create_connection(addr, 5)
+        readable, writable, exceptional = select.select([sock], [sock], [sock], 5)
+        message = "GET / HTTP/1.1\r\n\r\n"
+        sock.sendall(message)
+        data = sock.recv(4096)
+        print addr, data
+    except:
+        print 'time out testing %s:%d' % addr
+    finally:
+        print
+
+test(simpleserver)
+test(goagent)
+test(baidu)
+test(facebook)
+test(bank)
