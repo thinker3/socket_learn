35b0e26ab220ee48fc7285cb1764099559c6f55a
thinker3 <metathinkerk@gmail.com>
2013-08-03 23:24:23 +0800

    getsockname, getpeername, setsockopt

--- connect_ex.py
+++ connect_ex.py
@@ -6,17 +6,20 @@ twitter = ('twitter.com', 443)
 facebook = ('www.facebook.com', 80)
 bank = ('ibsbjstar.ccb.com.cn', 443)
 def connect_test(addr):
-    try:
-        soc = socket.socket(2)
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print soc.getsockname()
+        soc.bind(('', 8755))
+        print soc.getsockname()
         soc.settimeout(5)
         err = soc.connect_ex(addr)
-        print err, type(err)
+        print soc.getsockname()
+        print soc.getpeername() # socket.error: [Errno 107] Transport endpoint is not connected -- for barred sites or frequent access
         soc.close()
-    except socket.gaierror:
-        print 'cable error'
+        print '*' * 50
 
 connect_test(baidu)
-connect_test(facebook)
 connect_test(netease)
 connect_test(bank)
 connect_test(twitter)
+connect_test(facebook)
