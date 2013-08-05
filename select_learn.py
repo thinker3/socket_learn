a24190cbf18968b70637e758c57c15145b3067dc
thinker3 <metathinkerk@gmail.com>
2013-08-05 09:09:02 +0800

    no exceptional socks

--- select_learn.py
+++ select_learn.py
@@ -1,42 +1,39 @@
-import select
-import socket
+import select, socket, time
 
-simpleserver = ('127.0.0.1', 9999)
-goagent = ('127.0.0.1', 8088)
 baidu = ('www.baidu.com', 80)
-facebook = ('www.facebook.com', 443)
-bank = ('ibsbjstar.ccb.com.cn', 443)
 twitter = ('twitter.com', 443)
 weibo = ('weibo.com', 80)
-boxun = ('www.boxun.com', 80)
 
-def test(addr):
+def show(lis):
+    for sock in lis:
+        print sock.getsockname(),
+    print
+
+def test():
     try:
-        sock = socket.create_connection(addr, 3)
-        print sock
-        readable, writable, exceptional = select.select([sock], [sock], [sock], 3)
-        #message = "GET / HTTP/1.1\r\n\r\n"
-        message = "GET / HTTP/1.0\r\n\r\n"
-        sock.sendall(message)
-        data = sock.recv(2)
-        print addr, data
+        baidu_sock = socket.create_connection(baidu, 3)
+        weibo_sock = socket.create_connection(weibo, 3)
+        twitter_sock = socket.create_connection(twitter, 3)
+        message = "GET / HTTP/1.1\r\n\r\n"
+        #message = "GET / HTTP/1.0\r\n\r\n"
+        baidu_sock.sendall(message)
+        twitter_sock.sendall(message)
+        time.sleep(1)
+        socks = [baidu_sock, weibo_sock, twitter_sock]
+        readable, writable, exceptional = select.select(socks, socks, socks, 5)
+        show(readable)
+        show(writable)
+        show(exceptional)
     except socket.timeout:
-        print 'time out testing %s:%d' % addr
+        print 'time out' 
     except socket.error, e:
         print 'error %s' % e
     finally:
         print
-        try:
-            sock.close()
-        except:
-            pass
+        for sock in socks:
+            try:
+                sock.close()
+            except:
+                pass
 
-test(twitter)
-test(weibo)
-test(simpleserver)
-test(goagent)
-test(twitter)
-test(baidu)
-test(boxun)
-test(bank)
-test(facebook)
+test()
