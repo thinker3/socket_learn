ca745564ba9211cba79c8ebc7f356b012aa6b3e2
thinker3 <metathinkerk@gmail.com>
2013-11-27 14:50:06 +0800

    be explicit

--- connect_ex.py
+++ connect_ex.py
@@ -2,9 +2,11 @@ import socket
 
 baidu = ('www.baidu.com', 80)
 netease = ('www.163.com', 80)
-twitter = ('twitter.com', 443)
-facebook = ('www.facebook.com', 80)
 bank = ('ibsbjstar.ccb.com.cn', 443)
+twitter = ('twitter.com', 80)
+https_twitter = ('twitter.com', 443)
+facebook = ('www.facebook.com', 80)
+https_facebook = ('www.facebook.com', 443)
 def connect_test(addr):
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -14,7 +16,7 @@ def connect_test(addr):
         soc.settimeout(5)
         err = soc.connect_ex(addr)
         print soc.getsockname()
-        print soc.getpeername() # socket.error: [Errno 107] Transport endpoint is not connected -- for barred sites or frequent access
+        print soc.getpeername() # socket.error: [Errno 107] Transport endpoint is not connected -- for barred sites ??? or frequent access
         soc.close()
         print '*' * 50
 
@@ -22,4 +24,6 @@ connect_test(baidu)
 connect_test(netease)
 connect_test(bank)
 connect_test(twitter)
+connect_test(https_twitter)
 connect_test(facebook)
+connect_test(https_facebook)
--- create_connection_learn.py
+++ create_connection_learn.py
@@ -1,12 +1,29 @@
 import socket
 
 baidu = ('www.baidu.com', 80)
-zero = ('0.0.0.0', 9001)
+src_ip = ''
+port = 9001
+src_addr = (src_ip, port)
+
+allowed_src_ips = [
+        '',
+        '0.0.0.0',
+        '127.0.0.0',
+        '192.168.0.0',
+        '192.168.0.35',
+        ]
+
+problematic_src_ips = [
+        '127.0.0.1', # socket.error: [Errno 22] Invalid argument
+        '192.168.0.34', # socket.error: [Errno 99] Cannot assign requested address
+        ]
 
 def test(addr, src_addr):
     try:
         sock = socket.create_connection(addr, 3, src_addr)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # not working, netstat -na |grep 9001
         print sock.getsockname()
+        print sock.getpeername()
         print
         sock.sendall('GET / HTTP/1.1\r\n\r\n')
         data = sock.recv(1024)
@@ -17,5 +34,5 @@ def test(addr, src_addr):
         except:
             pass
 
-test(baidu, zero)
-#test(baidu, None)
+test(baidu, src_addr)
+#test(baidu, None) # invariant local ip, varying ports, such as ('192.168.0.35', 57365)
