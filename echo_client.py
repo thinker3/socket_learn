ccb2baafde2b772cf2cc2fa723bdb8b5a9026fb8
thinker3 <metathinkerk@gmail.com>
2013-12-16 17:53:15 +0800

    client port bind

--- echo_client.py
+++ echo_client.py
@@ -4,6 +4,7 @@ HOST = '192.168.0.8' # sshqn sshdb
 PORT = 8000 
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.bind(('', 1060)) # without this, random port will be assigned
 soc.connect((HOST, PORT))
 print 'Client has been assigned socket name', soc.getsockname()
 msg = sys.argv[1]
