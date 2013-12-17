a598ecb1d2354ef836a28c9258776c75d6ab97a7
thinker3 <metathinkerk@gmail.com>
2013-12-17 15:02:16 +0800

    show local address

--- proxy_learn/forbidden.py
+++ proxy_learn/forbidden.py
@@ -5,6 +5,7 @@ news = ('news.163.com', 80)
 def fetch(addr):
     try:
         sock = socket.create_connection(addr)
+        print sock.getsockname()
         sock.sendall('GET / HTTP/1.1\r\n\r\n')
         data = sock.recv(1024)
         print data
