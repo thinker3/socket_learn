931e5de7ee14624538fefb32f02d6793d4497db6
thinker3 <metathinkerk@gmail.com>
2014-12-20 16:11:59 +0800

    host2ip_conn_send_recv_client reviewed

--- host2ip_conn_send_recv_client.py
+++ host2ip_conn_send_recv_client.py
@@ -1,26 +1,48 @@
-import socket, time
+#!/usr/bin/env python
+# encoding: utf-8
 
+import socket
+
+
+def separate():
+    print '-' * 100
+
+separate()
 host = 'www.baidu.com'
 port = 80
-#message = "GET / HTTP/1.1\r\n\r\n" # HTTP/1.1 302 Moved Temporarily
-#message = "GET http://www.baidu.com/ HTTP/1.1\r\nHost: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0\r\nConnection: keep-alive\r\n\r\n" # socket.error: [Errno 104] Connection reset by peer
-message = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0\r\nConnection: keep-alive\r\n\r\n"
+buffersize = 1024
+buffersize = 1024 / 2
 
+message = "GET / HTTP/1.1\r\n\r\n"  # HTTP/1.1 302 Moved Temporarily
+# long string, long_string, longstr
+message = (
+    'GET / HTTP/1.1\r\n'
+    'Host: www.baidu.com\r\n'
+    'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0\r\n'
+    'Connection: keep-alive\r\n\r\n'
+)
+print message
+separate()
+print message,
+
+
+separate()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = socket.gethostbyname(host)
-print ip
-s.connect((ip, port))
+print ip  # such as 112.80.248.73
+#s.connect((ip, port))
+s.connect((host, port))  # both are ok
 s.sendall(message)
-#time.sleep(1)
-reply = s.recv(100 * 1024)
+reply = s.recv(buffersize)
 print reply
 s.close()
 
 
-print '-' * 100
+separate()
 s = socket.create_connection((host, port))
 s.sendall(message)
-time.sleep(1)
-reply = s.recv(100 * 1024)
+reply = s.recv(buffersize)
 print reply
 s.close()
+
+separate()
