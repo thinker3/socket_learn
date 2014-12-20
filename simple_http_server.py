71c4a0240c29f4a092f48160b9ff91055178f728
thinker3 <metathinkerk@gmail.com>
2014-12-20 15:26:02 +0800

    simple_http_server reviewed

--- simple_http_server.py
+++ simple_http_server.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import sys
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-if sys.argv[1:]: # slicing can prevent index out of range
+if sys.argv[1:]:  # slicing can prevent index out of range
     port = int(sys.argv[1])
 else:
     port = 9999
-server_address = ('127.0.0.1', port)
-server_address = ('', port)
-#server_address = ('0.0.0.0', port) # equal to empty string
+server_address = ('127.0.0.1', port)  # Serving HTTP on 127.0.0.1 port 9999 ...
+server_address = ('0.0.0.0', port)  # Serving HTTP on 0.0.0.0 port 9999 ...
+server_address = ('', port)  # Serving HTTP on 0.0.0.0 port 9999 ...
 
 httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
 socketname = httpd.socket.getsockname()
