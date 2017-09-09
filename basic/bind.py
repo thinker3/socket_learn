f6e9c480634854f4f18e93b4ee73e691676e4e31
thinker3 <penomivy@gmail.com>
2017-09-10 07:25:52 +0800

    udp bind

--- basic/bind.py
+++ basic/bind.py
@@ -8,7 +8,7 @@ https_baidu = ('www.baidu.com', 443)
 https_twitter = ('twitter.com', 443)
 
 
-def test_bind(addr):
+def test_tcp_bind(addr):
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     soc.settimeout(3)
@@ -26,6 +26,27 @@ def test_bind(addr):
     print '*' * 50
 
 
-test_bind(baidu)
-test_bind(https_baidu)
-test_bind(https_twitter)
+def test_udp_bind(binded_addr):
+    data = 'no reply'
+    target = ('127.0.0.1', 6666)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if binded_addr:
+        sock.bind(binded_addr)
+    sock.sendto(data, target)
+    print 'binded address:', sock.getsockname()
+    try:
+        sock.getpeername()
+    except socket.error as e:
+        # socket.error: [Errno 57] Socket is not connected
+        print type(e), e
+    sock.close()
+    print '*' * 50
+
+
+test_tcp_bind(baidu)
+test_tcp_bind(https_baidu)
+test_tcp_bind(https_twitter)
+
+test_udp_bind(None)
+test_udp_bind(('', 6660))
--- basic/fake_udp_server.py
+++
@@ -1,5 +0,0 @@
-import socket, sys
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto('Fake reply', ('127.0.0.1', int(sys.argv[1])))
-print 'server address', sock.getsockname()
