397bbd29b1ec5b2ecc7e4e52db52b9698a191566
thinker3 <penomivy@gmail.com>
2017-09-09 23:21:42 +0800

    getaddrinfo

--- basic/addrinfo.py
+++ basic/addrinfo.py
@@ -1,36 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import socket
 
 baidu = 'www.baidu.com'
 netease = 'www.163.com'
-twitter = 'twitter.com'
-facebook = 'www.facebook.com'
-bank = 'ibsbjstar.ccb.com.cn'
+
 
 def addrinfo_of(host, port=80):
     try:
-        a = socket.getaddrinfo(host, port)
-        print a
+        info = socket.getaddrinfo(host, port)
+        for one in info:
+            print one
+            (family, socktype, proto, canonname, sockaddr) = one
+            assert family == socket.AF_INET
+            assert (
+                socktype == socket.SOCK_STREAM and proto == socket.IPPROTO_TCP
+                or  # noqa
+                socktype == socket.SOCK_DGRAM and proto == socket.IPPROTO_UDP
+            )
+            assert canonname == ''
         print
     except Exception as e:
-        print e
+        print type(e), e
+
 
 def hostlist_of(host, port=80):
+    ips = set()
     x = socket.getaddrinfo(host, port)
     for one in x:
-        yield one[-1][0]
+        ip = one[-1][0]
+        ips.add(ip)
+    return list(ips)
 
 
-#addrinfo_of(baidu)
-#addrinfo_of(facebook)
-#addrinfo_of(netease)
-#addrinfo_of(bank)
-addrinfo_of(twitter, 443)
-twitter_ips = list(set(hostlist_of(twitter, 443)))
-baidu_ips = list(set(hostlist_of(baidu)))
+addrinfo_of(baidu)
+addrinfo_of(netease)
 
-print twitter_ips
+baidu_ips = hostlist_of(baidu)
 print baidu_ips
-print twitter_ips or baidu_ips
-print
-print [] or baidu_ips
-print None or baidu_ips
